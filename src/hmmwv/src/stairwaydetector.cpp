a1d0f327e5fb13e32e46fa1db2c6e8bb1d8fe77f
Max <mail@ne0h.de>
2015-12-14 13:43:32 +0100

    [stairs] much more plane detection

--- src/hmmwv/src/stairwaydetector.cpp
+++ src/hmmwv/src/stairwaydetector.cpp
@@ -1,16 +1,89 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 
-#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/visualization/cloud_viewer.h>
 
 using namespace std;
+ros::Publisher pub;
 
-void callback(const sensor_msgs::PointCloud2ConstPtr& cloud) {
-	ROS_INFO("Callback! %d %d", cloud->width, cloud->height);
+void callback(const sensor_msgs::PointCloud2ConstPtr& input) {
+	ROS_INFO("Callback! %d %d", input->width, input->height);
 
-	pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+	// convert from ros::pointcloud2 to pcl::pointcloud2
+	pcl::PCLPointCloud2* unfilteredCloud = new pcl::PCLPointCloud2;
+	pcl::PCLPointCloud2ConstPtr unfilteredCloudPtr(unfilteredCloud);
+	pcl_conversions::toPCL(*input, *unfilteredCloud);
+
+	// create a voxelgrid to downsample the input data to speed things up.
+	pcl::PCLPointCloud2::Ptr cloudBlob (new pcl::PCLPointCloud2);
+
+	pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+	sor.setInputCloud(unfilteredCloudPtr);
+	sor.setLeafSize(0.01f, 0.01f, 0.01f);
+	sor.filter(*cloudBlob);
+
+	// convert to pointcloud
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::fromPCLPointCloud2(*cloudBlob, *cloud);
+
+	// Does the parametric segmentation
+	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+
+	pcl::SACSegmentation<pcl::PointXYZ> seg;
+	seg.setOptimizeCoefficients(true);
+
+	seg.setModelType(pcl::SACMODEL_PLANE);
+	seg.setModelType(pcl::SACMODEL_PLANE);
+	seg.setMaxIterations(1000);
+	seg.setDistanceThreshold(0.01);
+
+	pcl::ExtractIndices<pcl::PointXYZ> extract;
+
+	// while 30% of the original cloud is still there
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud1(new pcl::PointCloud<pcl::PointXYZ>),
+		cloud2(new pcl::PointCloud<pcl::PointXYZ>);
+	unsigned int i = 0, pointsAtStart = cloud->points.size();
+	//while (cloud->points.size() > 0.3 * pointsAtStart) {
+
+		seg.setInputCloud(cloud);
+		seg.segment(*inliers, *coefficients);
+
+		if (inliers->indices.size() == 0) {
+			ROS_WARN("Could not estimate a planar model for the given dataset.");
+			//break;
+		}
+
+		// extract the inliers
+		extract.setInputCloud(cloud);
+		extract.setIndices(inliers);
+		extract.setNegative(false);
+		extract.filter(*cloud1);
+		std::cerr << "PointCloud representing the planar component: " << cloud1->width * cloud1->height
+			<< " data points." << std::endl;
+		std::cerr << "Model coefficients: " << coefficients->values[0] << " " 
+                                      << coefficients->values[1] << " "
+                                      << coefficients->values[2] << " " 
+                                      << coefficients->values[3] << std::endl;
+
+		extract.setNegative(true);
+		extract.filter(*cloud2);
+		cloud.swap(cloud2);
+
+		i++;
+	//}
+
+	//exit(0);
+
+	sensor_msgs::PointCloud2 output;
+	pcl::toROSMsg(*cloud1, output);
+	pub.publish(output);
 }
 
 int main(int argc, char **argv) {
@@ -18,6 +91,7 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "stairwaydetector");
 	ros::NodeHandle nh;
 	ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2>("/camera/depth/points", 1, callback);
+	pub = nh.advertise<sensor_msgs::PointCloud2>("/output", 1);
 	ros::spin();
 	
 	return 0;
